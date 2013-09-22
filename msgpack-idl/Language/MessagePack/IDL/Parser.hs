ea3fd07ac948ac6f0911e89658f3908d92b372f1
yfakariya <yfakariya@gmail.com>
2013-09-22 21:46:39 +0900

    Fix '\uXXXX ' style escape sequence is not supported in IDL.
    \uXXXX or \UXXXX is escaped form to specify a unicode code point in UTF-16 format in string literal, but its implementation didn't simply exist in peggy.
    This commit adds two helper functions to handle surrogate pairs correctly. Because haskell's Char type is UTF-32 sequence, so Data.Text.pack assumes that code points in surrogate range are always invalid (because they are reserved range and must not appear in UTF-32 sequence). However, this bahavior causes problem because:
    1) \uXXXX notation uses UTF-16 rather than UTF-32, so VALID surrogate pair can be appeared in string literals.
    2) The parser scans string literals, reads escaped A UTF-16 code point, and returns single Char, so it might be surrogate char.
    The helper function translates read Chars which contain UTF-16 value sequence into UTF-32 sequence.
    Note that the function does NOT handle invalid sequence because Data.Text.pack should reject invalid sequence, and parser should call it to create Literal value.

--- msgpack-idl/Language/MessagePack/IDL/Parser.hs
+++ msgpack-idl/Language/MessagePack/IDL/Parser.hs
@@ -4,11 +4,31 @@ module Language.MessagePack.IDL.Parser (
   idl,
   ) where
 
+import Data.Char
 import Data.Maybe
 import qualified Data.Text as T
+import Numeric
 import Text.Peggy
 import Text.Peggy.CodeGen.TH
 
 import Language.MessagePack.IDL.Syntax
+
+concatSurrogatePair :: [Char] -> [Char]
+concatSurrogatePair [] = []
+concatSurrogatePair (c : []) = [c]
+concatSurrogatePair (h : l : [])
+    | isSurrogatePair h l = [chr $ 0x10000 + ((ord h) - 0xD800) * 0x400 + ((ord l) - 0xDC00)]
+    | otherwise           = [h,l]
+concatSurrogatePair (h : l : rest) = (concatSurrogatePair [h,l]) ++ (concatSurrogatePair rest)
+
+isSurrogatePair :: Char -> Char -> Bool
+isSurrogatePair h l
+    | ucp < 0xD800 = False
+    | ucp > 0xDBFF = False
+    | lcp < 0xDC00 = False
+    | lcp > 0xDFFF = False
+    | otherwise    = True
+    where ucp = ord h
+          lcp = ord l
 
 genDecs $(peggyFile "mpidl.peggy")
